61c825cb07ad1a96ab09b5197bd32224abe8fb35
matteobonelli <matteobonelli08@gmail.com>
2023-12-20 16:28:50 +0100

    film page stylized

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,16 +5,19 @@
 @section('content')
 
 
-<div class="container my-4">
+<div class="container my-4" id='index-movies'>
     <div class="row">
         @foreach($movies as $movie)
         <div class="col-12 col-md-4 col-lg-3 gy-4">
             <a href="{{ route('movies.show', $movie->id)}}">
                 <div class="card">
                     <img src="{{$movie->image}}" alt="{{$movie->title}}">
-                    <h2>{{$movie->title}}</h2>
-                    <h5>{{$movie->original_title}}</h5>
-                    <p>{{$movie->language}} || {{$movie->vote}}</p>
+                    <div class="movie-container">
+                        <h2>{{$movie->title}}</h2>
+                        <h5>{{$movie->original_title}}</h5>
+                        <p>{{$movie->language}} || {{$movie->vote}}</p>
+                    </div>
+                    
                 </div>
             </a>
         </div>
